70dbb491017e9ace388342db4b756ca3f0123789
Piper Lastname <jazzedkirb@gmail.com>
2024-09-17 03:50:49 -0600

    Gravity prototype + input manager for controller &amp; keyboard support.

--- Assets/!/Prefabs/PlayerTest.cs
+++ Assets/!/Prefabs/PlayerTest.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]  
@@ -10,19 +10,41 @@ public class PlayerTest : MonoBehaviour
     public Rigidbody rb;
     [SerializeField]
     public float moveSpeed = 15f;
+    public PlayerInputActions playerControls;
+    public InputAction playerJump;
 
     Vector3 moveDirection = Vector3.zero;
 
+    private InputAction playerMove;
+    private InputAction playerFire;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        playerMove = playerControls.Player.Move;
+
+        playerMove.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerMove.Disable();
+    }
+
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+        /* float moveX = Input.GetAxis("Horizontal");
+         float moveZ = Input.GetAxis("Vertical");*/
 
-        moveDirection = new Vector3 (moveX, 0, moveZ).normalized;
+        moveDirection = new Vector3(playerMove.ReadValue<Vector2>().x, 0, playerMove.ReadValue<Vector2>().y) ;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(-moveDirection.x * moveSpeed, 0, -moveDirection.z * moveSpeed).normalized;
+        rb.velocity = new Vector3(-moveDirection.x * moveSpeed, 0, -moveDirection.z * moveSpeed);
     }
 }
